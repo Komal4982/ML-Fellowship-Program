16fc0292f8a1dacd15a2b3877c78f33e7991fefc
komal9482 <komalsalunkhe02@gmail.com>
2019-05-11 12:36:39 +0530

    BarChart File Updated

--- Week4/Numpy/ChangeDataTypeArray.py
+++ Week4/Numpy/ChangeDataTypeArray.py
@@ -3,8 +3,9 @@ x = np.array([[2, 4, 6], [6, 8, 10]], np.int32)
 print(x)
 print("Data type of the array x is:=", x.dtype)
 y = x.astype(float)
-print("New Value:=", y.dtype)
 print(y)
+print("New Value:=", y.dtype)
+
 
 
     
--- Week4/Numpy/Create2nd Array.py
+++ Week4/Numpy/Create2nd Array.py
@@ -1,5 +1,5 @@
 import numpy as np
-x = np.ones((5, 5), dtype = int)
+x = np.ones((5, 5), dtype=int)
 print("original array", x)
 x[1:-1, 1:-1] = 0
 print(x)
